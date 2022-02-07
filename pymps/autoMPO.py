af0e78f3608ee4743f75341f9b2b6715d32f3ee6
Daniel Miravet <dmiravet@gmail.com>
2022-02-07 12:52:41 -0500

    SVD to simplify auto MPO generated-1

--- pymps/autoMPO.py
+++ pymps/autoMPO.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
 import numpy as np
 import tensornetwork as tn
 import itertools as itt
@@ -9,52 +5,78 @@ from scipy import linalg as la
 import scipy as SP
 
 
-
-one = np.array([[1.0,0.0],[0,1.0]])
-sg = np.array([[1.0,0],[0,-1.0]])
-cdag = np.array([[0,1.0],[0,0]])
-c = np.array([[0,0],[1.0,0]])
-occ = np.array([1,0])
-emp = np.array([0,1])
-
-
-def SumOpTerms(op1, op2):
-    if(len(op1.shape)< 3):
-     dim = 2;
-     res = np.array([[[[0.]*2]*2]*dim]*dim)
-     res[0,0] = op1;
-     res[1,1] = op2;
-    if(len(op1.shape)>= 3):
-     dim = op1.shape[0] + 1
-     res = np.array([[[[0.]*2]*2]*dim]*dim)
-     res[0:dim-1,0:dim-1] = op1;
-     res[dim-1,dim-1] = op2;
-    return res
-
 class LocalOperator:
+    """A simple class to define auxiliary operators that support multiplication and addition,
+        based on the rules for MPO defined in  PHYSICAL REVIEW B 95, 035129 (2017)
+     
+     
+    """  
     def __init__(self, O, L):
-        self.O = O #np.array([[[0]*2]*2]*L)
+        self.O = O 
         self.L = L
+        
     def __mul__(self, op2):
-        if(type(op2)==float):
+        """Multiplication of two local operators or operator by a number(float)
+        Parameters
+        ----------
+        op2 : float or LocalOperator
+        operator to be multiplied     
+        
+        Returns
+        -------
+        new operator : LocalOperator
+        """
+        # operator by float multiplication
+        if(type(op2) == float): 
             coeff = abs(op2)**(1./self.L)
             sign = 0
             if abs(op2) > 1e-10:
              sign = op2/abs(op2)
             return LocalOperator(self.O*coeff*sign,self.L)
+        
+        # operator by operator multiplication
         res = self.O
         for j in range(self.L):
           res[j] = np.matmul(self.O[j],op2.O[j])
         return LocalOperator(res, self.L)
-    def __add__(self, op2):
-        Ovec = []
-        for j in range(self.L):
-            Ovec.append( [self.O[j],op2.O[j]])
-        return Ovec
+    
+#    def __add__(self, op2):
+#        """ Add two operator and append in diagonal, secc. IV in PHYSICAL REVIEW B 95, 035129 (2017)
+#        """
+#        Ovec = []
+#        for j in range(self.L):
+#            Ovec.append( [self.O[j],op2.O[j]])
+#        return Ovec
     
   
    
 def FermiOp(i, L, dagged):
+    """
+    A function that creates a local fermion operator in a way that
+    anticommutation relations are fulfilled. 
+
+    Parameters
+    ----------
+    i : int
+      site index
+    L : int
+      system length
+    dagged : int
+      equal 1 for creation operator and egual 0 for annihilation    
+    
+    Returns
+    -------
+    mps : LocalOperator
+        Local fermion operator
+
+    """
+    #Define local matrices
+    one = np.array([[1.0,0.0],[0,1.0]])
+    sg = np.array([[1.0,0],[0,-1.0]])
+    cdag = np.array([[0,1.0],[0,0]])
+    c = np.array([[0,0],[1.0,0]])
+    
+    #construct operators array to fulfill  anticommutation relations
     if dagged == 1:
        f = cdag
     else:
@@ -111,38 +133,33 @@ class Hamiltonian:
                         
          return H
  
-    def GetMPOTensors(self):
+    def GetMPOTensors(self, truncation_error = 1e-5):
          H = self.GetMPO()
          L = self.L
-         O0 = H[0]
-         hMPO = [tn.Node(O0,axis_names=["n_0p","n_0","i_0"] )]
          nterms = self.Ovec.shape[1]
+         
+         hMPO = [tn.Node(H[0], axis_names = ["n_0p","n_0","i_0"] )]         
          Oj = np.array([[[[0.]*nterms]*nterms]*2]*2)
-         for j in range(1,self.L-1):
+         for j in range(1, L-1):
              for n1 in range(2):
                  for n2 in range(2):
                   Oj[n1,n2] = np.diag(H[j,n1,n2])
-             hMPO += [tn.Node(Oj,axis_names=["n_{}p".format(j),"n_{}".format(j),"i_{}".format(j-1),"i_{}".format(j)])]
+             hMPO += [tn.Node(Oj, axis_names = ["n_{}p".format(j),"n_{}".format(j),"i_{}".format(j-1),"i_{}".format(j)])]
          
-         OL = H[self.L-1]
-         hMPO += [tn.Node(OL,axis_names=["n_{}p".format(L-1),"n_{}".format(L-1),"i_{}".format(L-2)] )]
+         hMPO += [tn.Node(H[L-1], axis_names = ["n_{}p".format(L-1),"n_{}".format(L-1),"i_{}".format(L-2)] )]
          
          
              
          #simplify tensors using SVD        
-         u, vh, trun_err = tn.split_node(hMPO[0], left_edges=[hMPO[0]["n_0p"],hMPO[0]["n_0"]],right_edges=[hMPO[0]["i_0"]],max_truncation_err=0.01,edge_name="g")
-         hMPO[0] = tn.Node(u.tensor,axis_names=["n_0p","n_0","i_0"] )
-         
+         u, vh, trun_err = tn.split_node(hMPO[0], left_edges = [hMPO[0]["n_0p"],hMPO[0]["n_0"]],right_edges=[hMPO[0]["i_0"]], max_truncation_err = truncation_error, edge_name = "g")
+         hMPO[0] = tn.Node(u.tensor, axis_names = ["n_0p","n_0","i_0"] )         
          for j in range(1,L-1):
             hMPO[j]= tn.Node(tn.ncon([vh.tensor,hMPO[j].tensor],[(-3,1),(-1,-2,1,-4)]),axis_names=["n_{}p".format(j),"n_{}".format(j),"i_{}".format(j-1),"i_{}".format(j)])
-            u, vh, trun_err = tn.split_node(hMPO[j], left_edges=[hMPO[j]["n_{}p".format(j)],hMPO[j]["n_{}".format(j)],hMPO[j]["i_{}".format(j-1)]],right_edges=[hMPO[j]["i_{}".format(j)]],max_truncation_err=0.01,edge_name="g")
-            print(j)
-            print(tn.shape(vh))
-            print(tn.shape(hMPO[j+1]))
+            u, vh, trun_err = tn.split_node(hMPO[j], left_edges = [hMPO[j]["n_{}p".format(j)],hMPO[j]["n_{}".format(j)],hMPO[j]["i_{}".format(j-1)]], right_edges = [hMPO[j]["i_{}".format(j)]], max_truncation_err = truncation_error, edge_name  ="g")
             hMPO[j] = tn.Node(u.tensor,axis_names=["n_{}p".format(j),"n_{}".format(j),"i_{}".format(j-1),"i_{}".format(j)])
          hMPO[L-1]= tn.Node(tn.ncon([vh.tensor,hMPO[L-1].tensor],[(-3,1),(-1,-2,1)]),axis_names=["n_{}p".format(L-1),"n_{}".format(L-1),"i_{}".format(L-2)])
          
-         connected_edges2=[]
+         connected_edges2 = []
          for j in range(1,L):
             conn2 = hMPO[j-1]["i_{}".format(j-1)]^hMPO[j]["i_{}".format(j-1)]
             connected_edges2.append(conn2)
